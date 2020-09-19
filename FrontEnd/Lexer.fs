a33e1de62f3b5bd83aeb187f994d150f7bb40ee0
HubCodes <krlrhkstk12@gmail.com>
2020-09-19 20:52:59 +0900

    Add: code location processing (each end of token)

--- FrontEnd/Lexer.fs
+++ FrontEnd/Lexer.fs
@@ -1,21 +1,33 @@
 module Fsgo.FrontEnd.Lexer
 
 open System
-
 open FSharpPlus
 open FSharpPlus.Data
 open Fsgo.Lang.Token
 open Fsgo.FrontEnd.StringAcc
 
-let peek = State (fun stream -> Seq.head stream, stream)
+let private peek =
+  State (fun (stream, loc) -> Seq.head stream, (stream, loc))
 
-let read = State (fun stream -> Seq.head stream, Seq.tail stream)
+let private read =
+  let updateState (stream, loc) =
+    let head = Seq.head stream
+    match head with
+    | '\n' -> head, (Seq.tail stream, Loc.nextLine loc)
+    | _ -> head, (Seq.tail stream, Loc.nextCol loc)
+  State updateState
 
-let private isIdentifierStart ch = Char.IsLetter ch
+let private tokenFromState tokenString tokenConstructor =
+  let readLocFromState (stream, loc) =
+    let result = Token (tokenConstructor tokenString, loc)
+    result, (stream, loc)
+  State readLocFromState
+
+let private isIdentifierStart ch = Char.IsLetter ch || ch = '_'
 
 let private isDigit ch = Char.IsDigit ch
 
-let private lexIdentifier =
+let lexIdentifier =
   let stringAcc = makeStringAcc ()
   let rec makeToken acc =
     monad {
@@ -23,17 +35,19 @@ let private lexIdentifier =
       | ch when isIdentifierStart ch || isDigit ch ->
         let! ch = read
         return! makeToken (acc >>= append ch)
-      | _ -> toString acc |> Identifier
+      | _ ->
+        let tokenStr = toString acc
+        return! tokenFromState tokenStr Identifier
     }
   makeToken stringAcc
 
-let private doLex =
+let doLex =
   monad {
     let! startChar = peek
     match startChar with
     | ch when isIdentifierStart ch -> return! lexIdentifier
-    | _ -> Identifier ""
+    // | _ -> Token (Identifier "", Loc.empty)
   }
 
 let lex (code: string) =
-  State.run doLex (String.toSeq code) |> fst |> Ok
+  State.run doLex (String.toSeq code, { Line = 0; Col = 0 }) |> Ok
--- Lang/Token.fs
+++ Lang/Token.fs
@@ -1,4 +1,16 @@
 module Fsgo.Lang.Token
 
-type Token =
+type Loc = { Line: int; Col: int }
+
+[<RequireQualifiedAccess>]
+module Loc =
+  let empty = { Line = 0; Col = 0 }
+  
+  let nextLine loc = { Line = loc.Line + 1; Col = 0 }
+  
+  let nextCol loc = { loc with Col = loc.Col + 1 }
+
+type RawToken =
   | Identifier of string
+
+type Token = RawToken * Loc
